dc5637c563ae17c39472bfdd3025b55df1cbd4c2
heart527 <1572910052@qq.com>
2020-08-04 15:50:46 +0800

    校园动态，成长记录-数据渲染+css+js

--- js/common.js
+++ js/common.js
@@ -2,26 +2,14 @@ $('.iconshouye').click(function(){
 	window.location.href = "index.html";
 });
 $('.iconshangcheng-copy').click(function(){
-	window.location.href = "mall.html";
+	window.location.href = "initiation.html";
 });
 $('.iconweibiaoti--copy').click(function(){
-	window.location.href = "mine.html";
+	window.location.href = "growthRecord.html";
 });
 $('.iconzuojiantou1').click(function(){
-	window.location.href = "mine.html";
+	window.location.href = "campusDynamic.html";
 });
 $('.iconchongwujiyang').click(function(){
-	window.location.href = "adopt.html"
-});
-$('.iconshoumailingyang').click(function(){
-	window.location.href = "fostercare.html";
-});
-$('.iconshoucang').click(function(){
-	window.location.href = "pair.html";
-});
-$('.icongouwuche').click(function(){
-	window.location.href = "shopping.html";
-});
-$('.iconshezhi').click(function(){
-	window.location.href = "setting.html";
+	window.location.href = "mime.html"
 });
