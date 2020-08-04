fefe99792b62cc2057880b536b8a530bf3f5d4b8
heart527 <1572910052@qq.com>
2020-08-04 11:09:42 +0800

    导航栏，成长记录

---
+++ js/common.js
@@ -0,0 +1,27 @@
+$('.iconshouye').click(function(){
+	window.location.href = "index.html";
+});
+$('.iconshangcheng-copy').click(function(){
+	window.location.href = "mall.html";
+});
+$('.iconweibiaoti--copy').click(function(){
+	window.location.href = "mine.html";
+});
+$('.iconzuojiantou1').click(function(){
+	window.location.href = "mine.html";
+});
+$('.iconchongwujiyang').click(function(){
+	window.location.href = "adopt.html"
+});
+$('.iconshoumailingyang').click(function(){
+	window.location.href = "fostercare.html";
+});
+$('.iconshoucang').click(function(){
+	window.location.href = "pair.html";
+});
+$('.icongouwuche').click(function(){
+	window.location.href = "shopping.html";
+});
+$('.iconshezhi').click(function(){
+	window.location.href = "setting.html";
+});
